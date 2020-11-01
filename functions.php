6cdaa14c22bce99a7ffcd77dbb8acc3c350cd442
Çağrı Üçkan <uckancagri@gmail.com>
2020-11-01 17:39:21 +0300

    empty space

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ if ( ! isset( $content_width ) ) {
 add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
 function special_nav_class($classes, $item){
      if( in_array('secondary', $classes) ){
-             $classes[] = 'active ';
+      $classes[] = 'active ';
      }
      return $classes;
 }
